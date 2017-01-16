17f2677b9a1061ebfdc0224bd36bdbdb0cf3eec1
koudaiii <cs006061@gmail.com>
2017-01-16 12:30:50 +0900

    Add add-user command

--- command/user.go
+++ command/user.go
@@ -3,7 +3,13 @@ package command
 import (
 	"fmt"
 	"strings"
+	"os"
+
+	"github.com/koudaiii/dockerepos/quay"
+	flag "github.com/spf13/pflag"
 )
+
+var role string
 
 type AddUserCommand struct {
 	Meta
@@ -14,8 +20,31 @@ type DeleteUserCommand struct {
 }
 
 func (c *AddUserCommand) Run(args []string) int {
-	// Write your code here
+	flags := flag.NewFlagSet("dockerepos", flag.ExitOnError)
 
+	flags.Usage = func() {
+		flags.PrintDefaults()
+	}
+
+	flags.StringVar(&role, "role", "read", "role to use for the user =  ['read', 'write', 'admin']. default: read")
+
+	if len(args) < 2 {
+		fmt.Fprintln(os.Stderr, c.Help())
+		os.Exit(1)
+	}
+
+	ss := strings.Split(args[0], "/")
+	if len(ss) != 3 {
+		fmt.Fprintln(os.Stderr, c.Help())
+		os.Exit(1)
+	}
+
+	repos, err := quay.AddPermission(ss[1], ss[2],"user",args[1],role)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "err: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Fprintf(os.Stdout, "Added! %v(%v) in quay.io/%v/%v\n", repos.Name, repos.Role,ss[1], ss[2])
 	return 0
 }
 
