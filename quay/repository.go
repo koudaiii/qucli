cbf89be462696766b3dcf7bbddbbfd98bafe13a0
koudaiii <cs006061@gmail.com>
2017-01-16 11:49:51 +0900

    Add DeleteRepository

--- quay/repository.go
+++ quay/repository.go
@@ -29,6 +29,25 @@ func GetRepository(namespace string, name string) (ResponseRepository, error) {
 	return repos, nil
 }
 
+func DeleteRepository(namespace string, name string) (ResponseRepository, error) {
+	repos := ResponseRepository{
+		Name: name,
+		Namespace: namespace,
+	}
+	u, err := url.Parse(QuayURLBase)
+	if err != nil {
+		return repos, err
+	}
+	u.Path = path.Join(u.Path, "repository", namespace, name)
+
+	_, err = utils.HttpDelete(u.String(), os.Getenv("QUAY_API_TOKEN"))
+	if err != nil {
+		return repos, err
+	}
+
+	return repos, nil
+}
+
 func CreateRepository(namespace string, name string, visibility string) (QuayRepository, error) {
 	var repos QuayRepository
 	req, err := json.Marshal(RequestRepository{
