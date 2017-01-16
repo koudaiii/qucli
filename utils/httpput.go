97814a85a6ac2abda14c462d8f21d19669b7e1f6
koudaiii <cs006061@gmail.com>
2017-01-16 12:12:43 +0900

    Add HttpPut

---
+++ utils/httpput.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+"bytes"
+"fmt"
+"io/ioutil"
+"net/http"
+)
+
+func HttpPut(url string, apitoken string, body []byte) (string, error) {
+	req, _ := http.NewRequest("PUT", url, bytes.NewBuffer([]byte(body)))
+	req.Header.Set("Content-Type", "application/json")
+	if apitoken != "" {
+		req.Header.Set("Authorization", "Bearer "+apitoken)
+	}
+
+	client := new(http.Client)
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("HTTP error!\nURL: %s\nstatus code: %d\nbody:\n%s\n", url, resp.StatusCode, string(b))
+	}
+
+	return string(b), nil
+}
