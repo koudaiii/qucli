baa5f3c80d457a965f5f6527cf23ececbb31470f
koudaiii <cs006061@gmail.com>
2017-09-29 10:43:00 +0900

    Add hostname option

--- command/flag.go
+++ command/flag.go
@@ -8,6 +8,7 @@ var (
 	role       string
 	visibility string
 	public     bool
+	hostname   string
 )
 
 func FlagInit(args []string) error {
@@ -20,6 +21,7 @@ func FlagInit(args []string) error {
 	flags.StringVar(&visibility, "visibility", "public", "visibility set to 'public' or 'private'.")
 	flags.StringVar(&role, "role", "read", "role to use for the user =  ['read', 'write', 'admin'].")
 	flags.BoolVar(&public, "is-public", true, "'--is-public=true' or '--is-public=false'.")
+	flags.StringVar(&hostname, "hostname", "quay.io", "If you use enterprise plan, set hostname option. ex '--hostname=quay.example.com'")
 
 	if err := flags.Parse(args[0:]); err != nil {
 		return err
