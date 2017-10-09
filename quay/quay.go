3abaa2aa5c3301aeba48a5813d547631e4ff72d9
koudaiii <cs006061@gmail.com>
2017-10-10 00:00:31 +0900

    Add type RepositoryNotification

--- quay/quay.go
+++ quay/quay.go
@@ -27,6 +27,40 @@ type ResponseRepositories struct {
 	Items []map[string]interface{} `json:"repositories"`
 }
 
+type RequestRepositoryNotification struct {
+	Title       string `json:"title"`
+	Event       string `json:"event"`
+	Method      string `json:"method"`
+	Config      []NotificationConfig
+	EventConfig []NotificationEventConfig
+}
+
+type NotificationConfig struct {
+	URL string `json:"url"`
+}
+
+type NotificationEventConfig struct {
+	EventConfig []map[string]interface{} `json:"eventConfig"`
+}
+
+type ResponseRepositoryNotifications struct {
+	Items []map[string]interface{} `json:"notifications"`
+}
+
+type RepositoryNotifications struct {
+	Items []RepositoryNotification
+}
+
+type RepositoryNotification struct {
+	Title            string                 `json:"title"`
+	Event            string                 `json:"event"`
+	Method           string                 `json:"method"`
+	EventConfig      map[string]interface{} `json:"event_config"`
+	UUID             string                 `json:"uuid"`
+	NumberOfFailures float64                `json:"number_of_failures"`
+	Config           map[string]interface{} `json:"config"`
+}
+
 type QuayRepository struct {
 	Namespace string `json:"namespace"`
 	Name      string `json:"name"`
